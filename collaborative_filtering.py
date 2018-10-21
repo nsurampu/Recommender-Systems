748e083b086f0b0fa1872b45331bb7fbaf70dba4
Naren Surampudi <nsurampu@gmail.com>
2018-10-21 17:12:28 +0530

    Updated collaborative_filtering.py

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,4 +1,5 @@
 import pickle
+import math
 
 movie_pickle = open("movie_file.txt", 'rb')
 rating_pickle = open("rating_file.txt", 'rb')
@@ -20,7 +21,8 @@ for user in userIds:
         user_rating_matrix[int(user)][int(movie)] = float(rating_dict[user][movie])
 
 test_user = 1
-pearson_similarity_dict = {}
+pearson_dict = {}
+last_movie = int(list(rating_dict[str(test_user)].keys())[-1]) + 1
 
 test_user_ratings = user_rating_matrix[test_user]
 total_test_ratings = sum(test_user_ratings)
@@ -35,14 +37,19 @@ for user in userIds:
     temp_numerator = 0
     temp_denominator_x = 0
     temp_denominator_y = 0
-    for movie in range(0, 193610):
+    for movie in range(1, last_movie):
         if (user_rating_matrix[test_user][movie] > 0) and (user_rating_matrix[user][movie] > 0):
             temp_numerator = temp_numerator + ((user_rating_matrix[test_user][movie] - mean_test_user_rating) * (user_rating_matrix[user][movie] - mean_user_rating))
             temp_denominator_x = temp_denominator_x + ((user_rating_matrix[test_user][movie] - mean_test_user_rating) ** 2)
             temp_denominator_y = temp_denominator_y + ((user_rating_matrix[user][movie] - mean_user_rating) ** 2)
-    temp_denominator = (temp_denominator_x ** 0.5) * (temp_denominator_y ** 0.5)
+    temp_denominator = math.sqrt(temp_denominator_x) * math.sqrt(temp_denominator_y)
     if temp_denominator > 0:
         coeff = temp_numerator / temp_denominator
-        pearson_similarity_dict[user] = coeff
+        pearson_dict[user] = coeff
 
-print(pearson_similarity_dict)
+# print(pearson_dict)
+sorted_pearson_dict = {t: pearson_dict[t] for t in sorted(pearson_dict, key=pearson_dict.get, reverse=True)}
+
+top_matches = {k:sorted_pearson_dict[k] for k in list(sorted_pearson_dict)[:6]}
+
+print(top_matches)
