b7596ed23a72300beffc9f20d7d9604b78008447
Naren Surampudi <nsurampu@gmail.com>
2018-10-21 18:10:55 +0530

    Updated collaborative_filtering.py

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -21,31 +21,36 @@ for user in userIds:
         user_rating_matrix[int(user)][int(movie)] = float(rating_dict[user][movie])
 
 test_user = 1
+test_user_rating = 0
 pearson_dict = {}
 last_movie = int(list(rating_dict[str(test_user)].keys())[-1]) + 1
 
-test_user_ratings = user_rating_matrix[test_user]
-total_test_ratings = sum(test_user_ratings)
-mean_test_user_rating = total_test_ratings / len(rating_dict[str(test_user)])
 # print(str(mean_test_user_rating))
 
 for user in userIds:
     user = int(user)
-    user_ratings = user_rating_matrix[user]
-    total_user_ratings = sum(user_ratings)
-    mean_user_rating = total_user_ratings / len(rating_dict[str(user)])
+    user_rating = 0
     temp_numerator = 0
     temp_denominator_x = 0
     temp_denominator_y = 0
-    for movie in range(1, last_movie):
+    length = 0
+    for movie in range(0, last_movie):
         if (user_rating_matrix[test_user][movie] > 0) and (user_rating_matrix[user][movie] > 0):
-            temp_numerator = temp_numerator + ((user_rating_matrix[test_user][movie] - mean_test_user_rating) * (user_rating_matrix[user][movie] - mean_user_rating))
-            temp_denominator_x = temp_denominator_x + ((user_rating_matrix[test_user][movie] - mean_test_user_rating) ** 2)
-            temp_denominator_y = temp_denominator_y + ((user_rating_matrix[user][movie] - mean_user_rating) ** 2)
-    temp_denominator = math.sqrt(temp_denominator_x) * math.sqrt(temp_denominator_y)
-    if temp_denominator > 0:
-        coeff = temp_numerator / temp_denominator
-        pearson_dict[user] = coeff
+            test_user_rating = test_user_rating + user_rating_matrix[test_user][movie]
+            user_rating = user_rating = user_rating + user_rating_matrix[user][movie]
+            length = length + 1
+    if length > 0:
+        mean_test_user_rating = test_user_rating / length
+        mean_user_rating = user_rating / length
+        for movie in range(1, last_movie):
+            if (user_rating_matrix[test_user][movie] > 0) and (user_rating_matrix[user][movie] > 0):
+                temp_numerator = temp_numerator + ((user_rating_matrix[test_user][movie] - mean_test_user_rating) * (user_rating_matrix[user][movie] - mean_user_rating))
+                temp_denominator_x = temp_denominator_x + ((user_rating_matrix[test_user][movie] - mean_test_user_rating) ** 2)
+                temp_denominator_y = temp_denominator_y + ((user_rating_matrix[user][movie] - mean_user_rating) ** 2)
+        temp_denominator = math.sqrt(temp_denominator_x) * math.sqrt(temp_denominator_y)
+        if temp_denominator > 0:
+            coeff = temp_numerator / temp_denominator
+            pearson_dict[user] = coeff
 
 # print(pearson_dict)
 sorted_pearson_dict = {t: pearson_dict[t] for t in sorted(pearson_dict, key=pearson_dict.get, reverse=True)}
