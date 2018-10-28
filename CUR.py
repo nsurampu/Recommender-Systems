2efee5d1fa9009bedfb0ef2752c23f1888d9bdb7
aditya-srikanth <adityashrikanth14@gmail.com>
2018-10-28 19:11:52 +0530

    made changes to CUR.py, need to check with test cases

--- CUR.py
+++ CUR.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import os
 from cur import cur_decomposition
+import time
 np.set_printoptions(threshold=np.inf)
 np.random.seed(30)
 
@@ -23,8 +24,11 @@ matrices C,U,R
 
 '''
 
-def CUR(A,num_dimensions,recomputeMatrix=False):
+def CUR(A,num_dimensions,recomputeMatrix=False,energy_needed=1.0):
     
+    if energy_needed > 1:
+        raise Exception('energy_needed should not exceed 1. The value of energy_needed was: {}'.format(energy_needed))
+
     file = 'CUR_Matrices.txt'
 
     if recomputeMatrix or not os.path.exists(file):
@@ -40,7 +44,7 @@ def CUR(A,num_dimensions,recomputeMatrix=False):
         column_scaling_factor = np.sqrt(p_column[selected_columns] * num_columns) 
         # print(temp_C.shape,len(column_scaling_factor))
         C = temp_C/column_scaling_factor
-        # print('C computed')
+        print('C computed')
 
         # Computing R matrix
         temp = np.power(A,2)
@@ -51,17 +55,16 @@ def CUR(A,num_dimensions,recomputeMatrix=False):
         rows_scaling_factor = np.sqrt(p_rows[selected_rows] * num_rows)
         R = temp_R/rows_scaling_factor
         R = R.T
-        # print('R ',R.shape)
-        # print('R computed')
+        print('R computed')
 
         # compute U
         W = A[selected_rows,:][:,selected_columns]
         # SVD for W
         W_WT = np.dot(W,W.T)
         WT_W = np.dot(W.T,W)
+        print('shapes',W_WT.shape,WT_W.shape)
         # eigenvalue decomposition of W WT
         eigenvalues_W_WT, X = np.linalg.eig(W_WT)
-        # print(eigenvalues_W_WT)
         idx = np.argsort(eigenvalues_W_WT)
         idx = idx[::-1]
         eigenvalues_W_WT = eigenvalues_W_WT[idx]
@@ -74,21 +77,44 @@ def CUR(A,num_dimensions,recomputeMatrix=False):
         idx = idx[::-1]
         eigenvalues_WT_W = eigenvalues_WT_W[idx]
         eigenvalues_WT_W[np.abs(eigenvalues_WT_W) <= 1e-10] = 1e200
-        # print(eigenvalues_WT_W[eigenvalues_WT_W == 0])
         Y = Y[:,idx]
         Y = Y.real
+
+        # energy based selection, if necessary that is...
+        if energy_needed != 1:        
+            variances =  np.power(eigenvalues_W_WT,2)
+            variances = variances.real
+            total_energy = np.sum(variances)
+            total_energy = total_energy.real
+            # print('total energy ',total_energy)
+            # print(variances[:10])
+            
+            index_to_slice = 0
+
+            for i in range(variances.shape[0]):
+                current_energy = np.sum(variances[:i])
+                if current_energy >= energy_needed*total_energy:
+                    index_to_slice = i
+                else:
+                    break
+            eigenvalues_WT_W = eigenvalues_WT_W[:index_to_slice + 1]
+            X = X[:,:index_to_slice + 1]
+            Y = Y[:,:index_to_slice + 1]
+
         Z_plus = np.eye(eigenvalues_WT_W.shape[0])
         Z_plus = Z_plus*1/eigenvalues_WT_W
         Z_plus[Z_plus == 1e-200] = 0 
         U = np.dot(Y,Z_plus)
         U = np.dot(U,X.T)
         U = U.real
+        eigenvalues_WT_W[np.abs(eigenvalues_WT_W) == 1e200] = 0
         # save file
         with open(file,'wb') as f:
             data = {}
             data['C'] = C
             data['R'] = R
-            data['U'] = U            
+            data['U'] = U     
+            data['eigenvalues'] = eigenvalues_WT_W       
             # save pickled data
             pickle.dump(data,f)
     else:
@@ -98,11 +124,44 @@ def CUR(A,num_dimensions,recomputeMatrix=False):
             C = data['C']
             R = data['R']
             U = data['U']
-            # print(C.shape,R.shape,U.shape) 
+            eigenvalues_WT_W = data['eigenvalues']
+            # print(C.shape,R.shape,U.shape)
 
-    return C,U,R
+        '''...check this...'''
+    # if energy_needed != 1:
+    #     energy = Energy(eigenvalues_WT_W)
+    #     print('energy ',energy)
+    #     # Reverse the eigenvalue np array
+    #     Reduction_array = np.empty([1])
+    #     for i in range(eigenvalues_WT_W.size,0,-1):
+    #         temp = eigenvalues_WT_W[0:i]
+    #         temp_Energy = Energy(temp)
+    #         if(temp_Energy >= energy_needed * energy):
+    #             Reduction_array = temp
+    #             print(i)
+    #         else:
+    #             break
+    #     size = Reduction_array.size
+    #     print('lite ',size)
+    #     print('zeros at',np.where(eigenvalues_WT_W == 0)[0])
+    #     print(C.shape,U.shape,R.shape)
+    #     Reduction_array = Reduction_array[0:(size-1)]
+    #     Reduction_array = Reduction_array.real
+    #     sigma_reduced = np.diag(Reduction_array)
+    #     C_reduced = C[:,0:(size-1)]
+    #     R_reduced = R[:,0:(size-1)]
+    #     RT_reduced = R_reduced.T
+    #     print('hehe ' ,C_reduced.shape,R_reduced.shape,sigma_reduced.shape)
+    #     new_A_reduced = np.dot(C_reduced,sigma_reduced)
+    #     ReducedA = np.dot(new_A_reduced,RT_reduced)
+    #     print(ReducedA.shape)
+    #     print(user_rating_matrix.shape)
+        
+    return C,U,R,eigenvalues_WT_W
+
+
 '''
-Calculates the reconstruction error incurred after CUR decomposition
+Calculates the Root mean Squared Error(RMSE) incurred after CUR decomposition
 
 Parameters:
 -----------
@@ -114,13 +173,59 @@ returns:
 error: reconstruction error incurred while decomposing
 
 '''
-def reconstructionError(originalMatrix,C,U,R):
+
+
+def rmse(originalMatrix,C,U,R):
     reconstructedMatrix = np.dot(C,U)
     reconstructedMatrix = np.dot(reconstructedMatrix,R)
 
-    error = np.sum(np.power((originalMatrix-reconstructedMatrix),2))
+    error = np.sum(np.power((originalMatrix-reconstructedMatrix),2))/(reconstructedMatrix.shape[0] * reconstructedMatrix.shape[1])
     error = np.power(error,0.5)
     return error
+
+def query(R,q):
+    start_time = time.clock()
+    temp = np.dot(q,R)
+    final = np.dot(temp,R.T)
+    duration = time.clock() - start_time
+    return final,duration
+
+def precisionTopK(k,q,R):
+    final = query(q,R)
+    print(final.shape)
+    final[final < 3.5] = 0
+    final[final > 3.5] = 1
+    q[q < 3.5] = 0
+    q[q > 3.5] = 1
+    idx = final.argsort()[::-1]
+    final = final[idx]
+    q = q[:,idx]
+    prec_val = 0
+    for i in range(0,k-1):
+        if(final[i,0] == 1 and q[i,0] == 1):
+            prec_val +=1
+    prec_val = prec_val / k
+    return prec_val
+
+''' 
+
+'''
+def spearmanCoefficient(predicted_rating,test_rating):
+    predicted_rank = np.argsort(predicted_rating)
+    test_rank = np.argsort(test_rating)
+    d = test_rank - predicted_rank
+    d_squared = np.power(d,2)
+    sum_d_squared = np.sum(d_squared)
+    n = d.shape[0]
+    rho = 1 - (6*sum_d_squared)/(n*(n**2 - 1))
+    return rho
+
+
+
+
+def Energy(A):
+    A = A*A
+    return A.sum()    
 
 if __name__=='__main__':
     movie_size = 2000           #INCLUSIVE OF 2000th movie
@@ -146,28 +251,29 @@ if __name__=='__main__':
             user_rating_matrix[int(user)][int(movie)] = float(rating_dict[user][movie])
 
     user_rating_matrix = np.array(user_rating_matrix)
-    min_error = 1e50
-    min_error_index = 100
-    # for i in range(1,100):
-    #     C,U,R = CUR(user_rating_matrix,605,recomputeMatrix=True)  # found experimentally.. assuming equal to the rank of the matrix
-    #     error = reconstructionError(user_rating_matrix,C,U,R)
+
+    # calculating best size (number of columns for fitting the data)
+    
+    # min_error = 1e50
+    # min_error_index = 0
+    # for i in range(1,user_rating_matrix.shape[0]):
+    #     C,U,R,eigenvalues = CUR(user_rating_matrix,605,recomputeMatrix=True)  # found experimentally.. assuming equal to the rank of the matrix
+    #     error = rmse(user_rating_matrix,C,U,R)
     #     if error < min_error:
     #         min_error = error
     #         min_error_index = i
     #     print(min_error,i)
-        
-    # print(min_error,min_error_index)
-    C,U,R = CUR(user_rating_matrix,605,recomputeMatrix=True)
-    error = reconstructionError(user_rating_matrix,C,U,R)
+    # print('min error at ',min_error,min_error_index)
+
+    C,U,R,eigenvalues = CUR(user_rating_matrix,605,recomputeMatrix=True,energy_needed=1)
+    error = rmse(user_rating_matrix,C,U,R)
     new_A = np.dot(np.dot(C,U),R)
-    print('\n\n',user_rating_matrix[:10,:10])
-    print(new_A[:10,:10])
-    print('\n\n')
     print('error my implementation ',error)
-    size_of_U = 370   # found experimentally
-    C,U,R = cur_decomposition(user_rating_matrix,size_of_U)
-    error = reconstructionError(user_rating_matrix,C,U,R)
-    print('error library ',error)
+    
+    # size_of_U = 370   # found experimentally
+    # C,U,R = cur_decomposition(user_rating_matrix,size_of_U)
+    # error = rmse(user_rating_matrix,C,U,R)
+    # print('error library ',error)
     # new_A = np.dot(np.dot(C,U),R)
     # print(user_rating_matrix[:10,:10])
     # print(new_A[:10,:10])
--- SVD.py
+++ SVD.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
-def energy(A):
+def Energy(A):
     A = A*A
     return A.sum()
 
@@ -11,16 +11,21 @@ def RMSE(user_array,FinalA):
     error = user_array - FinalA
     # error = error[1:,:]
     sqerror = error*error
-    print(sqerror.size)
+    # print(sqerror.size)
     RMSE = sqerror.sum()/(sqerror.size)
     RMSE = math.sqrt(RMSE)
-    print("RMSE is",RMSE)
+    return RMSE
 
 def SVD(user_array):
     UAT = user_array.T
     array_AAT = np.dot(user_array, (UAT))
     eigenvalues, eigenvectors_AAT = np.linalg.eig(array_AAT)
-    eigenvalues[eigenvalues < 1.0e-17] = 0
+    eigenvectors_AAT = eigenvectors_AAT.real
+    idx = eigenvalues.argsort()[::-1]
+    eigenvalues = eigenvalues[idx]
+    eigenvectors_AAT = eigenvectors_AAT[:,idx]
+
+    eigenvalues[eigenvalues < 1.0e-10] = 0
     #Finding rank
     rank = 0
     for i in eigenvalues:
@@ -28,23 +33,54 @@ def SVD(user_array):
         if(i.imag != 0):
             break
     rank = rank -1
+    rank = 610
     print("Rank: ",rank)
     # Rank = Row that contains last non zero value
     #Reducing size of eigenvalues to only include the actual rank
     eigenvalues = eigenvalues[0:(rank-1)]
-    eigenvalues = eigenvalues.real
+    # eigenvalues = eigenvalues.real
     #Build sigma
     sigma = np.diag(eigenvalues)
     #U of SVD with
     U = eigenvectors_AAT
     array_ATA = np.dot((UAT), user_array)
     eigenvalues_irr, eigenvectors_ATA = np.linalg.eig(array_ATA)
+    idx = eigenvalues_irr.argsort()[::-1]
+    eigenvalues_irr = eigenvalues_irr[idx]
+    eigenvectors_ATA = eigenvectors_ATA[:,idx]
     V = eigenvectors_ATA
     # Slicing to match Size
     U = U[:, 0:rank-1]
     V = V[:, 0:rank-1]
+    # print(sigma)
+    sigma = np.sqrt(sigma)
     print("Size of U,V",U.shape, V.shape)
     return U,sigma,V,eigenvalues
+
+def Query(q,V):
+    #Calculates the query result given a
+    temp = np.dot(q,V)
+    final = np.dot(temp,V.T)
+    return final
+
+def Precision_top_k(k,q,V):
+    final = Query(q,V)
+    print(final.shape)
+    final[final < 3.5] = 0
+    final[final > 3.5] = 1
+    q[q < 3.5] = 0
+    q[q > 3.5] = 1
+    idx = final.argsort()[::-1]
+    final = final[idx]
+    q = q[:,idx]
+    prec_val = 0
+    for i in range(0,k-1):
+        if(final[i,0] == 1 and q[i,0] == 1):
+            prec_val +=1
+    prec_val = prec_val / k
+    return prec_val
+
+
 
 movie_size = 2000  # INCLUSIVE OF 2000th movie
 user_size = 1500  # INCLUSIVE OF 1500th movie
@@ -75,20 +111,75 @@ user_array = user_array[1:,:]
 U,sigma,V,eigenvalues = SVD(user_array)
 
 VT = V.T
-U = U.real
-V = V.real
+# U = U.real
+# V = V.real
 new_A = np.dot(U,sigma)
 FinalA = np.dot(new_A,VT)
 
 print(FinalA.shape)
 
-energy = energy(eigenvalues)
+u,s,v = np.linalg.svd(user_array, full_matrices=True)
+# print(u)
+sn = np.diag(s)
+n = np.dot(u,sn)
+print("SVD Funct",u.shape,n.shape,sn.shape,v.shape)
+v = v[:,0:610]
+al = np.dot(n,v.T)
+print("SVD FUNCT",RMSE(user_array,al))
+
+
+energy = Energy(eigenvalues)
 # Reverse the eigenvalue np array
 Reduction_array = np.empty([1])
-for i in range(eigenvalue.size,0,-1):
-    temp = eigenvalue[0:i]
-    temp_energy = energy
-    # if()
+for i in range(eigenvalues.size,0,-1):
+    temp = eigenvalues[0:i]
+    temp_Energy = Energy(temp)
+    if(temp_Energy >= 0.9 * energy):
+        Reduction_array = temp
+    else:
+        break    
 
 
-error = RMSE(user_array,FinalA)
+size = Reduction_array.size
+print(size)
+Reduction_array = Reduction_array[0:(size-1)]
+Reduction_array = Reduction_array.real
+sigma_reduced = np.diag(Reduction_array)
+U_reduced = U[:,0:(size-1)]
+V_reduced = V[:,0:(size-1)]
+VT_reduced = V_reduced.T
+new_A_reduced = np.dot(U_reduced,sigma_reduced)
+ReducedA = np.dot(new_A_reduced,VT_reduced)
+
+
+print("Non reduced",RMSE(user_array,FinalA))
+print("90% reduced",RMSE(user_array,ReducedA))
+
+U_file = open("U_file.txt", 'wb')
+V_file = open("V_file.txt", 'wb')
+sigma_file = open("sigma_file.txt", 'wb')
+U_reduced_file = open("U_reduced_file.txt", 'wb')
+V_reduced_file = open("V_reduced_file.txt", 'wb')
+sigma_reduced_file = open("sigma_reduced_file.txt", 'wb')
+
+user_map = np.dot(U,sigma)
+sigma_map = np.dot(sigma,V.T)
+
+
+
+pickle.dump(U, U_file)
+pickle.dump(V, V_file)
+pickle.dump(sigma ,sigma_file)
+
+U_file.close()
+V_file.close()
+sigma_file.close()
+
+
+pickle.dump(U_reduced, U_reduced_file)
+pickle.dump(V_reduced, V_reduced_file)
+pickle.dump(sigma_reduced ,sigma_reduced_file)
+
+U_reduced_file.close()
+V_reduced_file.close()
+sigma_reduced_file.close()
